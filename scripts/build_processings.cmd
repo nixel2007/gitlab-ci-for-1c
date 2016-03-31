de4699410e96e1854c5703fba2550b019c9dbee2
Nikita Gryzlov <nixel2007@gmail.com>
2016-03-31 15:56:50 +0300

    Подгружена структура репозитория

---
+++ scripts/build_processings.cmd
@@ -0,0 +1,2 @@
+chcp 65001
+oscript -encoding=utf-8 ../vendor/precommit1c/v8files-extractor.os --compile "../src/processings" "../processings" --recursive
---
+++ scripts/decompose_processings.cmd
@@ -0,0 +1,4 @@
+chcp 65001
+cd ..
+oscript -encoding=utf-8 vendor/precommit1c/v8files-extractor.os --git-precommit "src" --remove-orig-bin-files
+cd scripts
